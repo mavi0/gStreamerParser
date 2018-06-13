ad3f75fec40ce640e638bfd6f9a92d9a1e1583fe
Eleanor Davies <ellie@mavieson.co.uk>
2018-06-13 13:11:09 +0100

    timentime

--- src/gstreamerparser/Driver.java
+++ src/gstreamerparser/Driver.java
@@ -5,39 +5,11 @@ import java.util.*;
 
 public class Driver
 {
-  public static final String CMD = "gst-launch-1.0 -vvv playbin uri='https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd' 2>&1 | tee >(ts '%d-%m-%y %H_%M_%S' > raw.log)";
+  public static final String CMD = "gst-launch-1.0 -vvv playbin uri='https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd' 2>&1 | tee >(ts '%Y-%m-%d %H_%M_%s' > raw.log)";
 
   public static void main(String[] args)
   {
-    // // stream the file, generate the file to be parsed
-    // try{
-    //   Process p = Runtime.getRuntime().exec(new String[]{"bash","-c", CMD});
-    //   p.waitFor();
-    // }
-    // catch (Exception e)
-    // {
-    //   e.printStackTrace();
-    // }
-
-    ArrayList<String> rawInput = new ArrayList<String>();
-
-    try
-    {
-      File file = new File("raw.log");
-      BufferedReader br = new BufferedReader(new FileReader(file));
-
-      String st;
-      while ((st = br.readLine()) != null)
-      {
-          rawInput.add(st);
-          // System.out.println(st);
-      }
-    }
-    catch (Exception e)
-    {
-      e.printStackTrace();
-    }
-
-
+    Parser p = new Parser();
   }
+
 }
--- src/gstreamerparser/TimeStamp.java
+++ src/gstreamerparser/TimeStamp.java
@@ -1,5 +1,12 @@
 package gstreamerparser;
 
 public class TimeStamp {
+  // private Date d;
+  // private Time t;
+  public TimeStamp(String rawInput)
+  {
+    
 
+    // 2018-06-13 11:01:12.1528884072
+  }
 }
