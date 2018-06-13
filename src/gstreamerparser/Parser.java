ffbf2cd85309097c3d81c59b0b4f60aa77786e3d
Eleanor Davies <ellie@mavieson.co.uk>
2018-06-13 15:13:24 +0100

    good log

--- src/gstreamerparser/Parser.java
+++ src/gstreamerparser/Parser.java
@@ -41,24 +41,35 @@ public class Parser
     // System.out.println(rawInput.get(1));
 
     Double startTime = parseTime(rawInput.get(0));
-    // bufferTime(0);
-    System.out.println(rawInput.get(570));
+    // String tst = "12-06-18 16:30:1528817458 Setting pipeline to PLAYING ...";
+    // String tst2 = tst.substring(46, 53);
+    // String p = "PLAYING";
+    // System.out.println(tst2 + tst2.equals(p));
+
+    bufferTime(0, startTime);
+    // System.out.println(rawInput.get(570));
     // System.out.println(rawInput.get(231).substring(47, 54));
 
   }
 // 12-06-18 16:30:1528817458 Setting pipeline to PLAYING ...
-  // private void bufferTime(int startLine)
-  // {
-  //   for (int i = startLine; i <= rawInput.size(); i++)
-  //   {
-  //     if (rawInput.get(i).substring(47, 54).equals("PLAYING"))
-  //     {
-  //       System.out.println("Found you ;)");
-  //       break;
-  //     }
-  //   }
-  //
-  // }
+  private void bufferTime(int startLine, double startTime)
+  {
+    for (int i = startLine; i <= rawInput.size(); i++)
+    {
+      // System.out.println(i);
+      if (rawInput.get(i).contains("PLAYING"))
+      {
+        // System.out.print("Bufffering Time ");
+        System.out.printf("Buffering Time %f\n", startTime);
+        System.out.printf("Buffering Time %f\n", parseTime(rawInput.get(i)));
+        System.out.printf("Buffering Time %f\n", parseTime(rawInput.get(i)) - startTime);
+        System.out.println(rawInput.get(i));
+        System.out.println("Found you ;)");
+        break;
+      }
+    }
+
+  }
 
   private double parseTime(String rawInput)
   {
@@ -70,7 +81,7 @@ public class Parser
     double epoch = localDateTime.atZone(zoneId).toEpochSecond();
     rawTime = "0" + rawTime.substring(19, 30);
     epoch = epoch + Double.parseDouble(rawTime);
-    // System.out.printf("dexp: %f\n", epoch);
+    //
     return epoch;
   }
 }
