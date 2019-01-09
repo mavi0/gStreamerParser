8d6f985646e54977829f0ab8c49b1ef337231597
Eleanor Davies <ellie@mavieson.co.uk>
2019-01-09 10:05:04 +0000

    test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-gst-launch-1.0 -vvv playbin uri='http://dash.akamaized.net/dash264/TestCasesHD/2b/qualcomm/2/MultiRes.mpd' 2>&1 | tee >(ts '%Y-%m-%d %H:%M:%.S' > raw.log.3)
+gst-launch-1.0 -vvv playbin uri='http://dash.akamaized.net/dash264/TestCasesHD/2b/qualcomm/2/MultiRes.mpd' 2>&1 | tee >(ts '%Y-%m-%d %H:%M:%.S' > raw.log)
+ant -Dlogfile=raw.log -Dmpd=testMPD/MultiRes.mpd
